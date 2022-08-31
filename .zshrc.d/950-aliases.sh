1c02734ef9ec7fad6e4e9fd9303f3eeed4f6f312
Matthew Jeffryes <matt@mjeffryes.net>
2022-08-30 20:33:52 -0700

    add alias for working on base dotfiles

--- .zshrc.d/950-aliases.sh
+++ .zshrc.d/950-aliases.sh
@@ -3,3 +3,7 @@ alias gka="gitk --all"
 alias gl="git log --oneline --decorate --graph origin/master HEAD"
 alias gla="git log --oneline --decorate --graph --all"
 alias gup='git checkout origin/master -b gup && git fetch && git checkout - && git branch -D gup'
+
+function .base {
+  git --git-dir="$HOME/.dotfiles/base" --work-tree="$HOME" "$@"
+}
