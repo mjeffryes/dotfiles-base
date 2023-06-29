cd0679d5f3ea16e1605ba8d3b4ea82b8eea3bef9
Matthew Jeffryes <matt@mjeffryes.net>
2023-06-28 17:34:20 -0700

    Fix unbound variable error when no failures in brew setup script, add failure tracking to setup scripts, add .base function to vim setup file to avoid depending on the environment

--- setup-scripts/osx/100-install-brew-stuff.sh
+++ setup-scripts/osx/100-install-brew-stuff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ue -o pipefail
+set -ueo pipefail
 
 # install brew
 which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -34,9 +34,9 @@ slack
 spotify
 "
 
-failed=()
+declare -a failed
 
-tempfile=`mktemp`
+tempfile=$(mktemp)
 
 remaining="$formulae"
 while [[ $remaining = *[![:space:]]* ]]; do
@@ -44,7 +44,7 @@ while [[ $remaining = *[![:space:]]* ]]; do
   echo "Running: brew install $remaining"
   brew install $remaining 2>&1 | tee "$tempfile" || fail=1
   last_tried=$(grep -E "(Installing (Cask|Formula))|(brew reinstall)" "$tempfile" | tail -n 1 | grep -oE '[^ ]+$')
-  if [[ $fail ]]; then failed+=($last_tried); fi
+  if [[ $fail ]]; then failed+=("$last_tried"); fi
   remaining="${remaining#*${last_tried}}"
 done
 
@@ -54,11 +54,11 @@ while [[ $remaining = *[![:space:]]* ]]; do
   echo "Running: brew install --cask $remaining"
   brew install --cask $remaining 2>&1 | tee "$tempfile" || fail=1
   last_tried=$(grep -E "(Installing (Cask|Formula))|(brew reinstall)" "$tempfile" | tail -n 1 | grep -oE '[^ ]+$')
-  if [[ $fail ]]; then failed+=($last_tried); fi
+  if [[ $fail ]]; then failed+=("$last_tried"); fi
   remaining="${remaining#*${last_tried}}"
 done
 
-if [[ $failed ]]; then
+if [[ ${#failed[@]} != 0 ]]; then
   echo Failures:
-  for f in $failed; do echo $f: FAILED; done
+  for f in "${failed[@]}"; do echo "$f": FAILED; done
 fi
