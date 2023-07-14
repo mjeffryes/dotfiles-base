4e2dc8c14fa97b6b3de623e68aca725946c5f6ca
Matthew Jeffryes <matt@mjeffryes.net>
2023-07-13 22:06:53 -0700

    better way to set up aliases for dotfiles git

--- .zshrc.d/950-aliases.sh
+++ .zshrc.d/950-aliases.sh
@@ -1,9 +1,14 @@
+#!/bin/zsh
+
 alias cd..="cd .."
 alias gka="gitk --all"
 alias gl="git log --oneline --decorate --graph origin/master HEAD"
 alias gla="git log --oneline --decorate --graph --all"
 alias gup='git checkout origin/master -b gup && git fetch && git checkout - && git branch -D gup'
 
-function .base {
-  git --git-dir="$HOME/.dotfiles/base" --work-tree="$HOME" "$@"
-}
+pushd "$HOME/.dotfiles/" 1>/dev/null || exit
+for repo in *; do
+  [[ $repo =~ ^[a-z]+$ ]] || continue
+    alias ".$repo=git --git-dir=\"$HOME/.dotfiles/$repo\" --work-tree=\"$HOME\""
+done
+popd 1>/dev/null|| exit
