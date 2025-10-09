14742c86a5ecf738466b17a3282a7f21775da930
Matthew Jeffryes <matt@mjeffryes.net>
2025-10-09 15:59:28 -0700

    Misc environment improvements
    - add zentracker and filesweeper
    - improve brew Aliases
    - add ~/.local/bin to PATH
    - use any avaialble gh command to get auth for github in gitconfig
    - use ambient $EDITOR for git editor
    - update some vim plugin pins

--- .zshrc.d/940-brew-aliases.sh
+++ .zshrc.d/940-brew-aliases.sh
@@ -1,9 +1,29 @@
 #!/bin/zsh
 
 # shadow the brew command to prevent accidental use
-alias brew="echo 'Please use brew_up or brew_add instead of brew directly'"
+brew()  {
+  if [[ "$1" == "up" || "$1" == "update" || "$1" == "upgrade" ]]; then
+    echo "Please use 'brew_up' instead of 'brew $1'"
+  elif [[ "$1" == "install" ]]; then
+    echo "Please use 'brew_add' instead of 'brew $1'"
+  else
+    command brew "$@"
+  fi
+}
+
+# escape hatch
 alias brew_for_real='command brew'
 
 # Aliases for managing Homebrew packages with a Brewfile
-alias brew_add='brew_for_real bundle add --file="$HOME/.config/homebrew/Brewfile'
-alias brew_up='brew_for_real bundle install --file=- < "$HOME"/.config/homebrew/Brewfile*'
+brew_up() {
+  # update our system to match the Brewfile
+  brew bundle install --file=- $@ < "$HOME"/.config/homebrew/Brewfile*
+  brew bundle cleanup --file=- < "$HOME"/.config/homebrew/Brewfile*
+
+}
+brew_add() {
+  # add a package to the Brewfile and install it
+  brew bundle add --file="$HOME/.config/homebrew/Brewfile" $@
+  brew bundle install --no-upgrade --file=- < "$HOME"/.config/homebrew/Brewfile*
+}
+
